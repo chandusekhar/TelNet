36c18b72944b7c18039deee69b07cfeb7304ece7
Emina Huskic <huskic.emina@gmail.com>
2017-05-25 02:52:07 +0200

    Slanje maila

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Mail;
 using System.Web;
 using System.Web.Mvc;
-
+using System.Web.UI;
+using TelNet.ViewModels;
 namespace TelNet.Controllers
 {
     public class HomeController : Controller
@@ -26,5 +28,34 @@ namespace TelNet.Controllers
 
             return View();
         }
+
+        [HttpPost]
+        public ActionResult Contact([Bind(Include = "ime,email,naslov,poruka")]
+        MailData podaci)
+        {
+            podaci = new MailData();
+            podaci.ime = String.Format("{0}", Request.Form["name"]);
+            podaci.email = String.Format("{0}", Request.Form["email"]);
+            podaci.naslov = String.Format("{0}", Request.Form["subject"]);
+            podaci.poruka = String.Format("{0}", Request.Form["message"]);
+            if (ModelState.IsValid)
+            {
+                MailMessage mail = new MailMessage();
+                SmtpClient SmtpServer = new SmtpClient("smtp.gmail.com");
+                mail.From = new MailAddress(podaci.email);
+                mail.To.Add("huskic.emina@gmail.com");
+                mail.Subject =podaci.naslov;
+                mail.Body = podaci.poruka;
+
+                SmtpServer.Port = 587;
+                SmtpServer.Credentials = new System.Net.NetworkCredential("huskic.emina@gmail.com", "Kristal1$");
+                SmtpServer.EnableSsl = true;
+
+                SmtpServer.Send(mail);
+               }
+
+           return View();
+        }
+       
     }
 }
---
+++ ViewModels/MailData.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TelNet.ViewModels
+{
+    public class MailData
+    {
+        public string ime;
+        public string email;
+        public string naslov;
+        public string poruka;
+
+    }
+}
